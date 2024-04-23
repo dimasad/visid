a3c42c8024723b690c77720dc5a9515b41710830
Dimas Abreu Archanjo Dutra <dimas.dutra@mail.wvu.edu>
2024-04-22 22:33:36 -0400

    Created new modules for vi and gvi.

---
+++ src/visid/gvi.py
@@ -0,0 +1,191 @@
+"""Gaussian variational inference for state-space models."""
+
+import abc
+from typing import Literal
+
+import flax.linen as nn
+import jax.flatten_util
+import jax.numpy as jnp
+import jax.scipy as jsp
+import jax_dataclasses as jdc
+from numpy.typing import NDArray
+
+from . import common, stats, vi
+from .vi import Data, SampleWeights, StatePathPosterior
+
+CovarianceRepr = Literal['log_chol', 'L_expD']
+"""Covariance matrix representation.
+
+- 'L_expD' stands for the `R = L @ expm(D) @ L.T` decomposition of the
+  covariance matrix, where L is unitriangular and D is diagonal.
+- 'log_chol' stands for the lower-triangular matrix logarithm of the Cholesky 
+  factor of the covariance matrix R, that is,
+  `R == expm(log_chol) @ expm(log_chol).T`.
+"""
+
+TriaRoutine = Literal['qr', 'chol']
+"""Matrix triangularization routine option."""
+
+
+@jdc.pytree_dataclass
+class GaussianStatePathBase(StatePathPosterior):
+    """Base class for Gaussian state-path posteriors."""
+
+    mu: NDArray
+    """State samples."""
+
+    def sample_marg(self, us_dev):
+        """Sample the states from the marginal posterior at each time index."""
+        dev = self.scale_marg_samples(us_dev)
+        if dev.ndim == 2:
+            dev = dev[:, None]
+        return self.mu + dev
+
+    def sample_pairs(self, next_us_dev, curr_us_dev):
+        """Sample the states from the marginal posterior at each time index."""
+        dev_pair = self.scale_marg_samples(next_us_dev, curr_us_dev)
+        if dev_pair[0].ndim == 2:
+            dev_pair = [dev[:, None] for dev in dev_pair]
+        xnext = self.mu[1:] + dev_pair[0]
+        xcurr = self.mu[:-1] + dev_pair[1]
+        return xnext, xcurr
+
+    @abc.abstractmethod
+    def scale_marg_samples(self, norm_dev):
+        """Scale normalized deviations from mean by the state marginals."""
+
+    @abc.abstractmethod
+    def scale_xpair_samples(self, norm_dev_next, norm_dev_curr):
+        """Scale normalized deviations from mean of consecutive state pairs."""
+
+
+@jdc.pytree_dataclass
+class GaussianSteadyStatePosterior(GaussianStatePathBase):
+    """Gaussian steady-state state-path using representation."""
+
+    mu: NDArray
+    """State path mean."""
+
+    cond_scale: tuple[NDArray, NDArray] | NDArray
+    """Scaling of the conditional state of a time sample, given the previous."""
+
+    cross: NDArray
+    """Normalized cross-correlation between consecutive states."""
+
+    cov_repr: jdc.Static[CovarianceRepr] = 'L_expD'
+    """Representation of the covariance matrix."""
+
+    tria = jdc.Static[TriaRoutine] = 'qr'
+    """Matrix triangularization routine."""
+
+    def scale_marg_samples(self, norm_dev):
+        return jnp.inner(norm_dev, self.chol_marg_cov)
+
+    def scale_xpair_samples(self, norm_dev_next, norm_dev_curr):
+        return (jnp.inner(norm_dev_curr, self.cross)
+                + jnp.inner(norm_dev_next, self.chol_cond_cov))
+    
+    @property
+    def chol_cond_cov(self):
+        """Cholesky factor of the conditional covariance."""
+        if self.cov_repr == 'L_expD':
+            L, d = self.cond_scale
+            L = jnp.tril(L, k=-1) + jnp.identity(len(d))
+            return L @ jnp.diag(jnp.exp(0.5 * d))
+        elif self.cov_repr == 'log_chol':
+            return jsp.linalg.expm(jnp.tril(self.cond_scale))
+        else:
+            raise ValueError("Invalid covariance representation.")
+
+    @property
+    def chol_marg_cov(self):
+        """Cholesky factor of the marginal covariance."""
+        if self.tria == 'qr':
+            return common.tria2_qr(self.chol_cond_cov, self.cross)
+        elif self.tria == 'chol':
+            return common.tria2_chol(self.chol_cond_cov, self.cross)
+        else:
+            raise ValueError("Invalid triangularization routine.")
+    
+    def entropy(self, xnext: NDArray, xcurr: NDArray, w: SampleWeights):
+        """Entropy of the state-path posterior."""
+        N = len(self.mu)
+        if self.cov_repr == 'L_expD':
+            L, d = self.cond_scale
+            return N / 2  * jnp.sum(d)
+        elif self.cov_repr == 'log_chol':
+            return N * jnp.trace(self.cond_scale)
+        else:
+            raise ValueError("Invalid covariance representation.")
+
+
+class LinearConvolutionSmoother(nn.Module):
+    """Linear convolution smoother."""
+
+    nwin: int
+    """Length of the convolution window."""
+
+    nx: int
+    """Number of states."""
+
+    conv_mode: Literal['full', 'same', 'valid'] = 'valid'
+    """Mode argument passed to `jax.numpy.convolve`."""
+
+    cov_repr: CovarianceRepr = 'L_expD'
+    """Representation of the covariance matrix."""
+
+    tria: TriaRoutine = 'qr'
+    """Matrix triangularization routine."""
+
+    def setup(self):
+        nx = self.nx
+        self.cross = self.param('cross', nn.initializers.zeros, (nx, nx))
+        if self.cov_repr == 'L_expD':
+            L = self.param('L', nn.initializers.zeros, (nx, nx))
+            d = self.param('d', nn.initializers.zeros, (nx,))
+            self.cond_scale = (L, d)
+        elif self.cov_repr == 'log_chol':
+            self.cond_scale = self.param('S', nn.initializers.zeros, (nx, nx))
+        else:
+            raise ValueError("Invalid covariance representation.")
+
+    @nn.compact
+    def __call__(self, data: Data):
+        """Apply the linear convolution smoother."""
+        # Retrieve and concatenate the convolution inputs
+        u = getattr(data, 'conv_u', data.u)
+        y = getattr(data, 'conv_y', data.y)
+        sig = jnp.c_[y, u].T
+
+        # Retrieve and initialize the convolution kernel
+        K_shape = (self.nx, len(sig), self.nwin)
+        K = self.param('K', nn.initializers.normal(), K_shape)
+
+        # Apply kernels and sum to obtain mean
+        mu = common.vconv(sig, K, mode=self.conv_mode).sum(1).T
+
+        return GaussianSteadyStatePosterior(
+            mu=mu, cond_scale=self.cond_scale, cross=self.cross,
+            cov_repr=self.cov_repr, tria=self.tria
+        )
+
+
+class SigmaSampler(nn.Module):
+    """Sigma point sampler for Gaussian distributions."""
+
+    nx: int
+    """Number of states."""
+
+    def marginals(self, posterior: GaussianStatePathBase, seed=None):
+        """Sample from the assumed density."""
+        us_dev, w = stats.sigmapts(self.nx)
+        x = posterior.sample_marg(us_dev)
+        return x, w
+
+    def pairs(self, posterior: GaussianStatePathBase, seed=None):
+        """Sample from the assumed density."""
+        us_dev, w = stats.sigmapts(2*self.nx)
+        next_us_dev = us_dev[:, :self.nx]
+        curr_us_dev = us_dev[:, self.nx:]
+        x = posterior.sample_pairs(next_us_dev, curr_us_dev)
+        return x, w
---
+++ src/visid/vi.py
@@ -0,0 +1,62 @@
+"""Base classes for variational inference in state-space models."""
+
+import abc
+from typing import Literal
+
+import flax.linen as nn
+import jax.flatten_util
+import jax.numpy as jnp
+import jax.scipy as jsp
+import jax_dataclasses as jdc
+from numpy.typing import NDArray
+
+from . import common, stats
+
+SampleWeights = NDArray | None
+"""Sample weights for computing expectation."""
+
+
+@jdc.pytree_dataclass
+class Data:
+    """Data for estimation problems."""
+
+    y: NDArray
+    """Measurements."""
+
+    u: NDArray
+    """Exogenous inputs."""
+
+
+@jdc.pytree_dataclass
+class StatePathPosterior(abc.ABC):
+
+    @abc.abstractmethod
+    def entropy(self, xnext: NDArray, xcurr: NDArray, w: SampleWeights):
+        """Entropy of the state posterior."""
+
+
+class VIBase:
+    """Base class for Variational Inference estimators."""
+
+    def elbo(self, data: Data, seed=None):
+        """Compute the Evidence Lower Bound."""
+        # Obtain the posterior assumed density
+        posterior = self.smoother(data)
+
+        # Sample from the assumed density
+        xmarg, wmarg = self.sampler.marginals(posterior, seed)
+        xpair, wpair = self.sampler.pairs(posterior, seed)
+
+        # Compute the elements of the complete-data log-density
+        trans = self.model.avg_path_trans_logpdf(*xpair, data.u, wmarg)
+        meas = self.model.avg_path_meas_logpdf(data.y, xmarg, data.u, wpair)
+
+        # Compute the entropy
+        entropy = posterior.entropy(*xpair, wpair)
+
+        # Add all terms and return
+        return trans + meas + entropy
+    
+    def __call__(self, data: Data, seed=None):
+        """Loss function for minimization."""
+        return -self.elbo(data, seed)
