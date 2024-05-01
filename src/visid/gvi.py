Fixed output masking.

src/visid/gvi.py
@@ -142,7 +142,8 @@ class LinearConvolutionSmoother(nn.Module):
         # Retrieve and concatenate the convolution inputs
         u = getattr(data, 'conv_u', data.u)
         y = getattr(data, 'conv_y', data.y)
-        sig = jnp.c_[y, u].T
+        y_masked = jnp.where(jnp.isnan(y), 0, y)
+        sig = jnp.c_[y_masked, u].T
 
         # Retrieve and initialize the convolution kernel
         K_shape = (self.nx, len(sig), self.nkern)

src/visid/modeling.py
@@ -111,9 +111,14 @@ class GaussianMeasurement(StochasticStateSpaceBase):
     def meas_logpdf(self, y, x, u):
         """Log-density of a measurement, log p(y_k | x_k, u_k)."""
         mean = self.h(x, u)
+        sigma = jnp.exp(self.meas_log_sigma)
+
+        # Mask out nans
         unmasked = ~jnp.isnan(y)
-        logpdf = jsp.stats.norm.logpdf(y, mean, jnp.exp(self.meas_log_sigma))
-        return jnp.sum(jnp.where(unmasked, logpdf, 0))
+        y_masked = jnp.where(unmasked, y, 0)
+
+        logpdf = jsp.stats.norm.logpdf(y_masked, mean, sigma)
+        return jnp.sum(unmasked * logpdf)
 
 
 class LinearModel(nn.Module):